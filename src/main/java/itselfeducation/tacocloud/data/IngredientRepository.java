b5fd51a33a6042724f8d2342c7f79862010432d8
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-08-20 20:41:40 +0300

    replace jdbc-template on DATA jdbc

--- src/main/java/itselfeducation/tacocloud/data/IngredientRepository.java
+++ src/main/java/itselfeducation/tacocloud/data/IngredientRepository.java
@@ -1,10 +1,9 @@
 package itselfeducation.tacocloud.data;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
+
 import java.util.Optional;
 
-public interface IngredientRepository {
-
-    Iterable<Ingredient> findAll();
-    Optional<Ingredient> findById(String id);
-    Ingredient save(Ingredient ingredient);
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 }
--- src/main/java/itselfeducation/tacocloud/data/OrderRepository.java
+++ src/main/java/itselfeducation/tacocloud/data/OrderRepository.java
@@ -1,7 +1,7 @@
 package itselfeducation.tacocloud.data;
 
 import itselfeducation.tacocloud.TacoOrder;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository {
-    TacoOrder save(TacoOrder order);
+public interface OrderRepository extends CrudRepository<TacoOrder,Long> {
 }
